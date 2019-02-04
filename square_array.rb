854bff85ed56b88dd80943377b06bd037ba1ac31
George Wong <gwong323@yahoo.com>
2019-02-04 21:47:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@
 def square_array(array) #build square_array method
 squared = []            #create new array named squared
 array.each do |number|  #take each number of array using temporary variable number
-squared << number ** 2  #squares each number and shovel into array squared
+squared << number ** 2  #squares each number and shovel into end array squared
 end #end for each
 squared #outputs array
 end #end for def
